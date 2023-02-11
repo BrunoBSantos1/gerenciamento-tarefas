960ad2b0fc97d17b0864501cd7f11e264a002520
Bruno Batista <brunobatysta.fla@gmail.com>
2023-02-11 20:02:18 -0300

    Corrigindo pasta do auth module

--- app-tarefas/src/app/auth/auth.module.ts
+++ app-tarefas/src/app/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
